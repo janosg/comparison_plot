2215196f5ba657f0f23d7e161831a9db83676430
Klara Roehrl <klara.roehrl@gmail.com>
2019-06-06 16:17:37 +0200

    Added confidence bands back in. Made them nearly invisible unless their model is selected. Implemented that only points can be selected by the TapTool.

--- comparison_plot.py
+++ comparison_plot.py
@@ -7,8 +7,6 @@ from bokeh.plotting import figure, show, output_notebook
 from bokeh.layouts import gridplot, column
 from bokeh.models.callbacks import CustomJS
 
-with open('callback.js', 'r') as f:
-    js_code = f.read()
 
 def comparison_plot(data_dict=None, df=None):
     """Make a comparison plot either from a data_dict or a DataFrame.
@@ -18,18 +16,18 @@ def comparison_plot(data_dict=None, df=None):
             The values are pd.DataFrames where the index contains the names of
             the parameters and there are three columns:
                 - 'params', containing the point estimates
-                - 'lower', containing the lower bound of the confidence interval
-                - 'upper', containing the upper bound of the confidence interval
+                - 'conf_int_lower', containing the lower bound of the confidence interval
+                - 'conf_int_upper', containing the upper bound of the confidence interval
 
         df (pd.DataFrame): DataFrame with columns
-            *model*, *param_value*, *param_name*, *lower*, *upper*, *group*
+            *model*, *param_value*, *param_name*, *conf_int_lower*, *conf_int_upper*, *group*
 
     """
     if df is None:
         df = _convert_res_dicts_to_df(res_dict=data_dict)
 
-    if 'group' not in df.columns:
-        df['group'] = 'all'
+    if "group" not in df.columns:
+        df["group"] = "all"
 
     output_notebook()
 
@@ -41,23 +39,22 @@ def comparison_plot(data_dict=None, df=None):
     source = ColumnDataSource(safe_df)
 
     plots = []
-    for group_name in df['group'].unique():
-
+    for group_name in df["group"].unique():
         # create the "canvas"
         group_plot = figure(
             title="Comparison Plot of {} Parameters".format(group_name.title()),
-            y_range=df[df['group'] == group_name]["param_name"].unique(),
+            y_range=df[df["group"] == group_name]["param_name"].unique(),
             plot_width=600,
             plot_height=300,
         )
 
         # add circles representing the parameter value
-        circle_glyph = group_plot.circle(
+        point_estimate_glyph = group_plot.circle(
             source=source,
             x="param_value",
             y="param_name",
             size=12,
-            color='#035096',
+            color="#035096",
             alpha=0.5,
             nonselection_color="#035096",
             selection_color="firebrick",
@@ -65,17 +62,34 @@ def comparison_plot(data_dict=None, df=None):
             selection_alpha=0.7,
         )
 
+        # add the confidence_intervals
+        # horizontal whiskers not supported in bokeh 1.0.4
+        conf_int_glyph = group_plot.hbar(
+            source=source,
+            y="param_name",
+            left="conf_int_lower",
+            right="conf_int_upper",
+            height=0.3,
+            alpha=0.0,
+            nonselection_fill_alpha=0.0,
+            selection_alpha=0.25,
+            line_alpha=0.1,
+        )
+
         # HoverTool
-        tooltips = [
-            ("parameter value", "@param_value"),
-            ("model", "@model")]
-        hover = HoverTool(renderers=[circle_glyph], tooltips=tooltips)
+        tooltips = [("parameter value", "@param_value"), ("model", "@model")]
+        hover = HoverTool(renderers=[point_estimate_glyph], tooltips=tooltips)
         group_plot.tools.append(hover)
 
         # TapTool
         js_kwargs = {"source": source}
+        with open("callback.js", "r") as f:
+            js_code = f.read()
         js_callback = CustomJS(args=js_kwargs, code=js_code)
-        tap = TapTool(callback=js_callback)
+        # point_estimate_glyph as only renderer assures that when a point is chosen
+        # only that point's model is chosen
+        # this makes it impossible to choose models based on clicking confidence bands
+        tap = TapTool(renderers=[point_estimate_glyph], callback=js_callback)
         group_plot.tools.append(tap)
 
         plots.append(group_plot)
@@ -86,7 +100,15 @@ def comparison_plot(data_dict=None, df=None):
 
 def _convert_res_dicts_to_df(res_dict):
     df = pd.DataFrame(
-        columns=["param_value", "lower", "upper", "model", "param_name", "std", "color"]
+        columns=[
+            "param_value",
+            "conf_int_lower",
+            "conf_int_upper",
+            "model",
+            "param_name",
+            "std",
+            "color",
+        ]
     )
 
     model_counter = 0
@@ -96,8 +118,10 @@ def _convert_res_dicts_to_df(res_dict):
         ext_param_df["param_name"] = ext_param_df.index
         # assuming that upper and lower are 95% CIs and using the 68-95-99.7 rule
         # https://en.wikipedia.org/wiki/68%E2%80%9395%E2%80%9399.7_rule
-        ext_param_df["std"] = (ext_param_df["upper"] - ext_param_df["lower"]) / 4
-        ext_param_df["color"] = "mediumelectricblue"
+        ext_param_df["std"] = (
+            ext_param_df["conf_int_upper"] - ext_param_df["conf_int_lower"]
+        ) / 4
+        ext_param_df["color"] = "#035096"
         df = df.append(ext_param_df, sort=False)
         model_counter += 1
 
