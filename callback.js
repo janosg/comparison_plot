e949c16793d5983d8e66e41a41184b442a4437f6
Klara Roehrl <klara.roehrl@gmail.com>
2019-06-06 12:54:41 +0200

    Added notebook showing stages of the function. Non-grouped plot now supports model selection on tapping point estimate.

--- callback.js
+++ callback.js
@@ -1,5 +1,23 @@
-var inds = src.selected.indices;
-if (inds.length == 0)
-    return;
+// adapted from https://stackoverflow.com/a/44996422
 
-alert(inds)
+var chosen = source.selected.indices;
+if (typeof(chosen) == "number"){
+    var chosen = [chosen]
+};
+
+var chosen_models = [];
+
+for (var i = 0; i < chosen.length; ++ i){
+    chosen_models.push(source.data['model'][chosen[i]])
+};
+
+var chosen_models_indices = [];
+
+for (var i = 0; i < source.data['index'].length; ++ i){
+    if (chosen_models.includes(source.data['model'][i])){
+        chosen_models_indices.push(i)
+    };
+};
+
+source.selected.indices = chosen_models_indices;
+source.change.emit();
